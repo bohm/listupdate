320d6d05fccda210bd27b552edc6a305cfd45c7a
Martin Böhm <boehm+git@cs.uni.wroc.pl>
2025-02-24 11:56:55 +0100

    Minor: converter fix.

--- short-to-int8t-conv.cpp
+++ short-to-int8t-conv.cpp
@@ -16,12 +16,12 @@ void PRINT_AND_ABORT(const char *format, ...) {
 }
 
 std::array<short, 3>* old_format = nullptr;
-std::array<uint8_t, 3>* new_format = nullptr;
+std::array<int8_t, 3>* new_format = nullptr;
 uint64_t advsize = 0;
 
 void init_arrays() {
     old_format = new std::array<short, 3>[advsize];
-    new_format = new std::array<uint8_t, 3>[advsize];
+    new_format = new std::array<int8_t, 3>[advsize];
     for (int i = 0; i < advsize; i++) {
       for (int j = 0; j < 3; j++) {
         old_format[i][j] = 0;
@@ -76,13 +76,12 @@ void serialize_last_three_int8t(const std::string& last_three_filename) {
 
         fclose(binary_file);
 
-        fprintf(stderr, "0: %hd, %hd, %hd.\n",
+        fprintf(stderr, "0: %" PRIi8 ", %" PRIi8 ", %" PRIi8 ".\n",
             new_format[0][0], new_format[0][1], new_format[0][2]);
-        fprintf(stderr, "1: %hd, %hd, %hd.\n",
+        fprintf(stderr, "1: %" PRIi8 ", %" PRIi8 ", %" PRIi8 ".\n",
             new_format[1][0], new_format[1][1], new_format[2][2]);
-        fprintf(stderr, "2: %hd, %hd, %hd.\n",
+        fprintf(stderr, "2: %" PRIi8 ", %" PRIi8 ", %" PRIi8 ".\n",
             new_format[2][0], new_format[2][1], new_format[2][2]);
-
     }
 
 int main(void)
